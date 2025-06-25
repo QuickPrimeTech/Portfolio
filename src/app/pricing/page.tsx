0da9d4c5bfaa66842162d740c5e8a60009a5aec0
QuickPrimeTech <quickprimetech@gmail.com>
2025-06-25 22:05:18 +0300

    Added a padding to the pricing cards

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -4,12 +4,15 @@ import AddOns from "@/sections/pricing/add-ons";
 import Faqs from "@/sections/pricing/faqs";
 import Hero from "@/sections/pricing/hero";
 import MorePricing from "@/sections/pricing/more-pricing";
+import { Section } from "@/components/typography";
 
 export default function PricingPage() {
   return (
     <main className="bg-background">
       <Hero />
-      <PricingCards />
+      <Section id="pricing" aria-labell="pricing-card">
+        <PricingCards />
+      </Section>
       <MorePricing />
       <AddOns />
       <Faqs />
